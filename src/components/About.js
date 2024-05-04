c495693dfda329414fbf76a53c51105c0976641d
Japheth <jkarani171@gmail.com>
2024-05-04 07:06:33 -0600

    good

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,28 @@
 import React from "react";
+import user from "../data/user";
 
-function About() {
+
+
+function Links(props) {
+  console.log(props.links);
+
+  return (
+    <div>
+      <h3>Links</h3>
+      <a href={props.github}>{props.github}</a>
+      <a href={props.linkedin}>{props.linkedin}</a>
+    </div>
+  );
+}
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio && <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {/* add your <Links /> component here */
+        <Links github={user.links.github} linkedin={user.links.linkedin} />
+      }
     </div>
   );
 }
